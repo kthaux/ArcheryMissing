2cbbbdfa29c1f017d4946db0d072dfb48cf03613
Sean Osborne <srosborn@ucsc.edu>
2022-06-06 16:06:13 -0700

    Audio trimming

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -7,16 +7,19 @@ class Load extends Phaser.Scene
 
     preload() 
     {
+        //background
         this.load.image('bg', 'assets/titletext.png');
         this.load.image('prepbg', 'assets/test.png');
         this.load.image('done', 'assets/ready.png');
         
+        //menu assets
         this.load.image('archerHat', 'assets/hat.png');
         this.load.image('upArrow', 'assets/upArrow.png');
         this.load.image('downArrow', 'assets/downArrow.png');
         this.load.image('minus', 'assets/minus.png');
         this.load.image('plus', 'assets/plus.png');
 
+        //sabotages
         this.load.image('horn', 'assets/horn.png');
         this.load.image('misbow', 'assets/misbow.png');
         this.load.image('fan', 'assets/fan.png');
@@ -24,6 +27,7 @@ class Load extends Phaser.Scene
         this.load.image('bentarrow', 'assets/brokenarrow.png');
         this.load.image('fight', 'assets/fight.png');
         
+        //audio
         this.load.audio('select', 'assets/selection.wav');                  // made via https://sfbgames.itch.io/chiptone
         this.load.audio('highSelect', 'assets/highselection.wav');
         this.load.audio('hit1', 'assets/hit1.wav');
